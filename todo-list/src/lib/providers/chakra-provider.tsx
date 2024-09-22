907ccd49aa0cae66171f90f264c4036317b0158e
Benjamin Ikeji <benjaminikeji@gmail.com>
2024-09-22 00:24:31 -0400

    Fix dark mode

--- todo-list/src/lib/providers/chakra-provider.tsx
+++ todo-list/src/lib/providers/chakra-provider.tsx
@@ -1,6 +1,5 @@
 import {
   ChakraProvider as BaseChakraProvider,
-  ColorModeScript,
   cookieStorageManager,
 } from '@chakra-ui/react';
 import type React from 'react';
@@ -13,24 +12,18 @@ type ChakraProps = {
 
 export const ChakraProvider = ({ children }: ChakraProps) => {
   return (
-    <>
-      <ColorModeScript
-        initialColorMode={customTheme.config?.initialColorMode}
-        type="cookie"
-      />
-      <BaseChakraProvider
-        colorModeManager={cookieStorageManager}
-        theme={customTheme}
-        toastOptions={{
-          defaultOptions: {
-            position: 'bottom-right',
-            duration: 5000,
-            isClosable: true,
-          },
-        }}
-      >
-        {children}
-      </BaseChakraProvider>
-    </>
+    <BaseChakraProvider
+      colorModeManager={cookieStorageManager}
+      theme={customTheme}
+      toastOptions={{
+        defaultOptions: {
+          position: 'bottom-right',
+          duration: 5000,
+          isClosable: true,
+        },
+      }}
+    >
+      {children}
+    </BaseChakraProvider>
   );
 };
